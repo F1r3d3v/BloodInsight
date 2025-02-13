f9e7bcdd34ee83a1e2d79c35f9d6ad21abe8b623
F1r3D3v <ogieniewski.dev@gmail.com>
2025-02-13 20:14:35 +0100

    fix tests

---
+++ test/unit_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Unit Tests', () {
+    test('Sample test', () {
+      expect(true, isTrue);
+    });
+  });
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Widget Tests', () {
+    testWidgets('Empty widget test', (tester) async {});
+  });
+}
