6af0be43cd608ba4d069e94490c0807bf83de54b
F1r3D3v <ogieniewski.dev@gmail.com>
2025-03-04 23:39:22 +0100

    Add environment configuration and caching for CI workflow

---
+++ lib/core/config/environment.dart
@@ -0,0 +1,4 @@
+final class Environment {
+  static const String googleMapsAPI = String.fromEnvironment('GOOGLE_MAPS_API');
+  static const String geminiAPI = String.fromEnvironment('GEMINI_API');
+}
